537ca8337776aeb514578abcb881ec6da174b883
cayan701 <cayan701@gmail.com>
2024-06-20 06:02:37 +0530

    Add new files

--- Tic-Tac-Toe/src/App.jsx
+++ Tic-Tac-Toe/src/App.jsx
@@ -3,11 +3,26 @@ import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from './winning-combinations.js';
 
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function App() {
   const [activePlayer, setActivePlayer] = useState('X');
   const [gameTurns, setGameTurns] = useState([]);
+
+  const gameBoard = initialGameBoard;
+    
+   for(const turn of gameTurns) {
+      const { square, player } = turn;
+      const { row, col } = square;
+        
+      gameBoard[row][col] = player;
+    }  
 
   function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((currPlayer) => currPlayer === 'X' ? 'O' : 'X');
