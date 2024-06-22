b1fc8db8894a22d9a77e72d9056e954e88904266
Ayan Chatterjee <106116707+cayan701@users.noreply.github.com>
2024-06-23 01:13:17 +0530

    Update Player.jsx

--- Tic-Tac-Toe/src/components/Player.jsx
+++ Tic-Tac-Toe/src/components/Player.jsx
@@ -9,7 +9,7 @@ function Player({ initialName, symbol, isActive }) {
     }
 
     function handleChange(event) {
-        console.log(event);
+        //console.log(event);
         setPlayerName(event.target.value);
     }
 
